5e8034a1681748300eb2574d23b52ece940b9b7f
zaziemo <maren.heltsche@gmail.com>
2018-03-06 14:26:45 +0100

    fix tests

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -5,8 +5,8 @@ fixtures :products
 
   test "add two different products" do
     cart = Cart.create
-    cart.add_product(products :ruby).save!
-    cart.add_product(products :another_book).save!
+    cart.add_product(products(:ruby)).save!
+    cart.add_product(products(:two)).save!
 
     assert_equal cart.line_items.size, 2
     assert_equal cart.line_items[0].quantity, 1
@@ -15,8 +15,8 @@ fixtures :products
 
   test "add two items of the same product" do
     cart = Cart.create
-    cart.add_product(products :ruby).save!
-    cart.add_product(products :ruby).save!
+    2.times { cart.add_product(products(:ruby)).save! }
+    # cart.add_product(products(:ruby)).save!
 
     assert_equal cart.line_items.size, 1
     assert_equal cart.line_items[0].quantity, 2
