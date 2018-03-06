90c7907ab525f380bc2f19eb2695eb051278e7d3
zaziemo <maren.heltsche@gmail.com>
2018-03-06 12:42:05 +0100

    add unit tests for adding two different items and two times the same item to a cart

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+fixtures :products
+
+  test "add two different products" do
+    cart = Cart.create
+    cart.add_product(products :ruby).save!
+    cart.add_product(products :another_book).save!
+
+    assert_equal cart.line_items.size, 2
+    assert_equal cart.line_items[0].quantity, 1
+    assert_equal cart.line_items[1].quantity, 1
+  end
+
+  test "add two items of the same product" do
+    cart = Cart.create
+    cart.add_product(products :ruby).save!
+    cart.add_product(products :ruby).save!
+
+    assert_equal cart.line_items.size, 1
+    assert_equal cart.line_items[0].quantity, 2
+  end
 end
