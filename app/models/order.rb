44a83e943657c35510ea28c99c57373e26141f72
Maren Heltsche <maren.heltsche@myclimate.org>
2018-04-26 15:32:55 +0200

    make tests pass again
    and notification notification if user continues shopping

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,9 +8,9 @@ class Order < ApplicationRecord
   }
 
   validates :name, :address, :email, presence: true
-  validates :pay_type, inclusion: pay_type.keys
+  validates :pay_type, inclusion: pay_types.keys
 
-  def add_line_items_from_cart
+  def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
       item.cart_id = nil
       line_items << item
