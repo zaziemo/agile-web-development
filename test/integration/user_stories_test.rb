f7eb2cd417be6fb3e9f5dbcb3da83e7d86ede474
Maren Heltsche <maren.heltsche@myclimate.org>
2018-05-07 15:22:02 +0200

    add different output formats to mailer
    and add integration test (which does not work yet properly)

---
+++ test/integration/user_stories_test.rb
@@ -0,0 +1,73 @@
+require 'test_helper'
+
+class UserStoriesTest < ActionDispatch::IntegrationTest
+  fixtures :products
+  include ActiveJob::TestHelper
+
+
+#---
+# Excerpted from "Agile Web Development with Rails 5",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails5 for more book information.
+#---
+
+  test "buying a product" do
+    start_order_count = Order.count
+    ruby_book = products(:ruby)
+
+    get "/"
+    assert_response :success
+    assert_select 'h1', "Your Pragmatic Catalog"
+
+    p ruby_book
+    p ruby_book.id
+    post '/line_items', params: { product_id: products(:ruby).id }, xhr: true
+    assert_response :success
+
+    cart = Cart.find(session[:cart_id])
+    assert_equal 1, cart.line_items.size
+    assert_equal ruby_book, cart.line_items[0].product
+
+    get "/orders/new"
+    assert_response :success
+    assert_select 'legend', 'Please Enter Your Details'
+
+    perform_enqueued_jobs do
+      post "/orders", params: {
+        order: {
+          name: "Dave Thomas",
+          address: "123 The Street",
+          email: "dave@example.com",
+          payment_type: payment_type
+        }
+      }
+
+      follow_redirect!
+
+      assert_response :success
+      assert_select 'h1', "Your Pragmatic Catalog"
+      cart = Cart.find(session[:cart_id])
+      assert_equal 0, cart.line_items.size
+
+      assert_equal start_order_count + 1, Order.count
+      order = Order.last
+
+      assert_equal "Dave Thomas",      order.name
+      assert_equal "123 The Street",   order.address
+      assert_equal "dave@example.com", order.email
+      assert_equal 1,                  order.payment_type_id
+
+      assert_equal 1, order.line_items.size
+      line_item = order.line_items[0]
+      assert_equal ruby_book, line_item.product
+
+      mail = ActionMailer::Base.deliveries.last
+      assert_equal ["dave@example.com"], mail.to
+      assert_equal ["depot@example.com"], mail.from
+      assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    end
+  end
+end
