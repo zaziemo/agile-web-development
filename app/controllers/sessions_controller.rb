9c6336bc67dce06edb833c42c837ec196908af1d
Maren Heltsche <maren.heltsche@myclimate.org>
2018-06-19 15:40:24 +0200

    make password changing work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
   skip_before_action :authorize
+  before_action :set_user, only: :create
 
   def new
   end
 
   def create
-    user = User.find_by(name: params[:name])
-    if user.try(:authenticate, params[:password])
-      session[:user_id] = user.id
+    if @user.try(:authenticate, params[:password])
+      session[:user_id] = @user.id
       redirect_to admin_url
     else
       redirect_to login_url, alert: "Invalid user/password combination"
@@ -18,5 +18,19 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
 
     redirect_to store_index_url, notice: "Logged out"
+  end
+
+  private
+
+  def set_user
+    @user = if User.all.empty?
+      User.create!(
+        name: params[:name],
+        password: params[:password],
+        password_confirmation: params[:password]
+      )
+    else
+      User.find_by(name: params[:name])
+    end
   end
 end
