2855a003e7058c9c5759b923e6f3e2d7967cbd35
Maren Heltsche <maren.heltsche@myclimate.org>
2018-09-14 10:13:39 +0200

    Remove comments from controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,29 +6,20 @@ class OrdersController < ApplicationController
   before_action :ensure_cart_isnt_empty, only: :new
   before_action :set_order, only: [:show, :edit, :update, :destroy, :ship]
 
-
-  # GET /orders
-  # GET /orders.json
   def index
     @orders = Order.all
   end
 
-  # GET /orders/1
-  # GET /orders/1.json
   def show
   end
 
-  # GET /orders/new
   def new
     @order = Order.new
   end
 
-  # GET /orders/1/edit
   def edit
   end
 
-  # POST /orders
-  # POST /orders.json
   def create
     @order = Order.new(order_params)
     @order.add_line_items_from_cart(@cart)
@@ -47,8 +38,6 @@ class OrdersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /orders/1
-  # PATCH/PUT /orders/1.json
   def update
     respond_to do |format|
       if @order.update(order_params)
@@ -61,8 +50,6 @@ class OrdersController < ApplicationController
     end
   end
 
-  # DELETE /orders/1
-  # DELETE /orders/1.json
   def destroy
     @order.destroy
     respond_to do |format|
@@ -82,12 +69,10 @@ class OrdersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_order
       @order = Order.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
       params.require(:order).permit(:name, :address, :email, :payment_type_id, :ship_date)
     end
--- app/controllers/payment_types_controller.rb
+++ app/controllers/payment_types_controller.rb
@@ -1,28 +1,20 @@
 class PaymentTypesController < ApplicationController
   before_action :set_payment_type, only: [:show, :edit, :update, :destroy]
 
-  # GET /payment_types
-  # GET /payment_types.json
   def index
     @payment_types = PaymentType.all
   end
 
-  # GET /payment_types/1
-  # GET /payment_types/1.json
   def show
   end
 
-  # GET /payment_types/new
   def new
     @payment_type = PaymentType.new
   end
 
-  # GET /payment_types/1/edit
   def edit
   end
 
-  # POST /payment_types
-  # POST /payment_types.json
   def create
     @payment_type = PaymentType.new(payment_type_params)
 
@@ -37,8 +29,6 @@ class PaymentTypesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /payment_types/1
-  # PATCH/PUT /payment_types/1.json
   def update
     respond_to do |format|
       if @payment_type.update(payment_type_params)
@@ -51,8 +41,6 @@ class PaymentTypesController < ApplicationController
     end
   end
 
-  # DELETE /payment_types/1
-  # DELETE /payment_types/1.json
   def destroy
     @payment_type.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class PaymentTypesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_payment_type
       @payment_type = PaymentType.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def payment_type_params
       params.require(:payment_type).permit(:name)
     end
