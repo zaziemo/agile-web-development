54e0f2bb4be0e69f3cd64569674eca89f8ca9e91
Maren Heltsche <maren.heltsche@myclimate.org>
2018-09-14 14:23:07 +0200

    Try to add different authentication
    for mime type other than http

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,16 @@ class ApplicationController < ActionController::Base
   protected
 
   def authorize
-    unless User.find_by(id: session[:user_id])
-      redirect_to login_url, notice: 'Please log in'
+    if request.format == 'text/html'
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: 'Please log in'
+      end
+    else
+      authenticate_or_request_with_http_basic do |username, password|
+        user = User.find_by(name: username)
+        ActiveSupport::SecurityUtils.secure_compare(username, user.name) &&
+        ActiveSupport::SecurityUtils.secure_compare(password, user.password)
+      end
     end
   end
 end
